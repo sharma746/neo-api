5c843cf93637f688d4ddd341ed7df4e30ed9b037
sharma746 <sharma.gautam746@gmail.com>
2021-06-01 14:48:49 +0530

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}} ">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- Styles -->
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <div>
+
+
+    </div>
+
+    <script src="{{asset('js/app.js')}} "></script>
+	
+</body>
+
+</html>
